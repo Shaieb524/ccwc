674b5f36a9a00136ed4e2c732d873a1c74dd1b18
Joud Shaieb <joudshaieb524@gmail.com>
2024-06-20 01:28:57 +0400

    read bytes & lines

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args) 
+    {
+        try 
+        {
+            var cmdOption = args[0].ToLower();
+            var inputFilePath = args[1];
+
+            var inputFile = new FileInfo(inputFilePath);
+
+
+            switch (cmdOption)
+            {
+                case "-cc":
+                    Console.WriteLine($"{inputFile.Length} {inputFile.Name}");
+                    break;
+                
+                case "-l":
+                    Console.WriteLine($"{File.ReadAllLines(inputFilePath).Count()} {inputFile.Name}");
+                    break;
+            
+            }
+        } 
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Please make sure to provide option and file path with the command input");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e}");
+        }
+        
+    } 
+
+}
+
